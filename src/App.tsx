224b181d9749e32c8b5782bb32779d1e07bbcd98
Francis Calizo <francisacalizo@gmail.com>
2020-06-09 11:15:49 -0400

    Get date using date-fns library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+
 import CurrentWeather from './components/CurrentWeather';
 import Location from './components/Location';
 
 function App() {
+  const [today, setToday] = useState('');
   const [weatherData, setWeatherData] = useState<IWeatherData>(null);
   const [forecasts, setForecasts] = useState<IForecasts>(null);
   const [city, setCity] = useState<string>('Miami');
+
+  // Get Today's Date
+  useEffect(() => {
+    setToday(format(new Date(), 'yyyy-MM-dd'));
+  }, []);
 
   // Fetch Current Weather
   useEffect(() => {
